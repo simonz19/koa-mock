248cf74a2320101f12f8213439d3848dce256add
zouxiaoya <576490177@qq.com>
2018-11-27 11:09:00 +0800

    tips

--- src/tpl/.koamockrc.js
+++ src/tpl/.koamockrc.js
@@ -2,6 +2,6 @@ module.exports = {
     port: 8081,
     endpoint: "/api",
     mode: "mock", //mock,proxy,auto
-    proxyTo: "" // worked on proxy mode only
+    proxyTo: "" // worked on proxy and auto mode only
   };
   
